d24fc0c39b112c17b379e135ab22179c5fb2a962
franPzTo <franperezt1@gmail.com>
2026-02-18 14:28:55 -0300

    Update routes: auth, user. Controls: auth. Utils: createAdmin

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { register, login, verifyEmail, logout } = require('../controllers/auth.controller');
+const { register, login, verifyEmail, logout, getUserProfile, updateProfilePhoto, updateUserProfile} = require('../controllers/auth.controller');
 const { uploadProfilePic } = require('../config/multer');
 const { validateRegister, validateLogin, validateVerifyEmail } = require('../middlewares/validator');
 const { authLimiter } = require('../middlewares/rateLimiter');
@@ -14,5 +14,9 @@ router.post('/verify-email', authLimiter, validateVerifyEmail, verifyEmail);
 
 // private endpoints
 router.post('/logout', verifyAuth, logout)
+router.get('/profile', verifyAuth, getUserProfile)
+router.put('/profile/photo', verifyAuth, uploadProfilePic, updateProfilePhoto)
+router.put('/profile/updateUser', verifyAuth, updateUserProfile)
+
 
 module.exports= router
